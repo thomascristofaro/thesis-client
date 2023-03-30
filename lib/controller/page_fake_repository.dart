e333a98bd94420163288bd913e9024501922d58e
Thomas Cristofaro <thomas.cristofaro@horsa.it>
2023-03-31 01:36:15 +0200

    fix

--- lib/controller/page_fake_repository.dart
+++ lib/controller/page_fake_repository.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:thesis_client/controller/layout.dart';
 import 'package:thesis_client/controller/page_interface.dart';
+import 'package:thesis_client/controller/record.dart';
 
 class PageFakeRepository implements IPageRepository {
   @override
@@ -11,5 +12,55 @@ class PageFakeRepository implements IPageRepository {
         await rootBundle.loadString('assets/page/$pageId/page_layout.json');
     final Map<String, dynamic> map = await json.decode(file);
     return Layout.fromMap(map);
+  }
+
+  @override
+  Future<void> delete(String pageId, Map<String, dynamic> filter) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Record>> get(String pageId, Map<String, dynamic> filter) async {
+    // implementare il filtro
+    final String file =
+        await rootBundle.loadString('assets/page/$pageId/page_data.json');
+    final Map<String, dynamic> map = await json.decode(file);
+    List<Map<String, dynamic>> list = map['recordset'];
+    return list.map((e) => Record.fromMap(e)).toList();
+  }
+
+  @override
+  Future<List<Record>> getAll(String pageId) async {
+    final String file =
+        await rootBundle.loadString('assets/page/$pageId/page_data.json');
+    final Map<String, dynamic> map = await json.decode(file);
+    List<Map<String, dynamic>> list = map['recordset'];
+    return list.map((e) => Record.fromMap(e)).toList();
+  }
+
+  @override
+  Future<Record?> getOne(String pageId, Map<String, dynamic> filter) async {
+    // implementare il filtro e prendere quello che trovi
+    final String file =
+        await rootBundle.loadString('assets/page/$pageId/page_data.json');
+    final Map<String, dynamic> map = await json.decode(file);
+    List<Map<String, dynamic>> list = map['recordset'];
+    return list
+        .map((e) => Record.fromMap(e))
+        .toList()
+        .first; // da prendere quello giusto
+  }
+
+  @override
+  Future<void> insert(String pageId, Record record) {
+    // TODO: implement insert
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> update(String pageId, Record record) {
+    // TODO: implement update
+    throw UnimplementedError();
   }
 }
--- lib/controller/page_interface.dart
+++ lib/controller/page_interface.dart
@@ -1,5 +1,13 @@
 import 'package:thesis_client/controller/layout.dart';
+import 'package:thesis_client/controller/record.dart';
 
 abstract class IPageRepository {
   Future<Layout> getLayout(String pageId);
+  Future<List<Record>> getAll(String pageId);
+  Future<List<Record>> get(String pageId, Map<String, dynamic> filter);
+  Future<Record?> getOne(String pageId, Map<String, dynamic> filter);
+  // TODO come gestire il valore di ritorno dell'API
+  Future<void> insert(String pageId, Record record);
+  Future<void> update(String pageId, Record record);
+  Future<void> delete(String pageId, Map<String, dynamic> filter);
 }
